86a8eccc07af75242351daab652182c4e8742ec3
ici <ici@DESKTOP-J9CT40H>
2022-12-13 17:39:08 +0900

    수정

--- src/WinBookInsert.java
+++ src/WinBookInsert.java
@@ -24,8 +24,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Vector;
+
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JCheckBox;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class WinBookInsert extends JDialog {
 
@@ -36,10 +40,11 @@ public class WinBookInsert extends JDialog {
 	private JTextField tfpDate;
 	private JTextField tfPrice;
 	private JTextField tfISBN;
-	private String imgText;
-	private JTextArea tfContent;
-	private String picURL; 
 
+	private String picURL;
+	private JTextArea taContents;
+	private JCheckBox ckOnce;
+	private JLabel lblPic;
 	/**
 	 * Launch the application.
 	 */
@@ -58,139 +63,133 @@ public class WinBookInsert extends JDialog {
 	 */
 	public WinBookInsert() {
 		setTitle("도서 등록 창");
-		setBounds(100, 100, 607, 662);
+		setBounds(100, 100, 600, 621);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
-		imgText = "<html><img src='";
-		imgText = imgText + "https://shopping-phinf.pstatic.net/main_3246352/32463527641.20221019105938.jpg"; //URL로 이미지 넣는법
-		imgText = imgText + "' width=150 height=200></html>"; // 이미지 사이즈 넣기
-		JLabel lblPic = new JLabel(imgText);
+		String imgText = "<html><img src='";
+		imgText = imgText + "https://shopping-phinf.pstatic.net/main_3248718/32487182777.20221019152842.jpg";
+		imgText = imgText + "' width=150 height=200></html>";
+		lblPic = new JLabel(imgText);
 		lblPic.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				 if(e.getClickCount()==2 ) {
-					 picURL = JOptionPane.showInputDialog("그림주소를 입력하시오");
-					 lblPic.setText("<html><img src='"+picURL+"' width=150 height=200></html>");
-				 }
-				
+				if(e.getClickCount()==2) {
+					picURL = JOptionPane.showInputDialog("그림 주소를 입력하시오");
+					lblPic.setText("<html><img src='"+picURL+"' width=150 height=200></html>");
+				}
 			}
 		});
+		
 		lblPic.setOpaque(true);
-		lblPic.setBackground(new Color(255, 255, 128));
-		lblPic.setBounds(24, 34, 150, 200);
+		lblPic.setBackground(new Color(255, 255, 0));
+		lblPic.setBounds(12, 10, 150, 200);
 		contentPanel.add(lblPic);
 		
-		JLabel lblTitle = new JLabel("책 제목 :");
-		lblTitle.setBounds(203, 42, 57, 15);
+		JLabel lblTitle = new JLabel("책 제목:");
+		lblTitle.setBounds(191, 13, 57, 15);
 		contentPanel.add(lblTitle);
 		
 		tfTitle = new JTextField();
-		tfTitle.setBounds(272, 39, 295, 25);
+		tfTitle.setBounds(256, 10, 316, 21);
 		contentPanel.add(tfTitle);
 		tfTitle.setColumns(10);
 		
-		JLabel lblAuthor = new JLabel("\uC800\uC790 :");
-		lblAuthor.setBounds(203, 85, 57, 15);
+		JLabel lblAuthor = new JLabel("저자:");
+		lblAuthor.setBounds(191, 53, 57, 15);
 		contentPanel.add(lblAuthor);
 		
 		tfAuthor = new JTextField();
 		tfAuthor.setColumns(10);
-		tfAuthor.setBounds(272, 82, 295, 25);
+		tfAuthor.setBounds(256, 50, 316, 21);
 		contentPanel.add(tfAuthor);
 		
-		JLabel lblPublisher = new JLabel("\uCD9C\uD310\uC0AC :");
-		lblPublisher.setBounds(203, 126, 57, 15);
+		JLabel lblPublisher = new JLabel("출판사:");
+		lblPublisher.setBounds(191, 94, 57, 15);
 		contentPanel.add(lblPublisher);
 		
 		tfPublisher = new JTextField();
+		tfPublisher.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+					String word = tfPublisher.getText().trim();
+					
+					try {
+						Class.forName("com.mysql.cj.jdbc.Driver");
+						Connection con = 
+								DriverManager.getConnection(
+										"jdbc:mysql://localhost:3306/sqlDB",
+										"root",
+										"12345");
+						Statement stmt = con.createStatement();			
+						String sql = "select distinct publisher from booktbl";
+						sql = sql + " where publisher like '" + word + "%'";
+						ResultSet rs = stmt.executeQuery(sql);
+						int cnt=0;
+						Vector<String> v = new Vector<>();
+						while(rs.next()) {	
+							v.add(rs.getString("publisher"));
+							System.out.println(rs.getString("publisher"));
+							cnt++;
+						}
+						if(cnt == 1)
+							tfPublisher.setText(v.get(0));
+						else {
+							WinPublisher winPublisher = new WinPublisher(v);
+							winPublisher.setModal(true);
+							winPublisher.setVisible(true);
+							
+							tfPublisher.setText(winPublisher.getPublisher());
+							tfpDate.requestFocus();
+						}
+					} catch (ClassNotFoundException | SQLException e1) {
+						e1.printStackTrace();
+					}
+					
+				}
+			}
+		});
 		tfPublisher.setColumns(10);
-		tfPublisher.setBounds(272, 123, 159, 25);
+		tfPublisher.setBounds(256, 91, 158, 21);
 		contentPanel.add(tfPublisher);
 		
-		JLabel lbpDate = new JLabel("\uCD9C\uD310\uC77C :");
-		lbpDate.setBounds(203, 172, 57, 15);
-		contentPanel.add(lbpDate);
+		JLabel lblpDate = new JLabel("출판일:");
+		lblpDate.setBounds(191, 138, 57, 15);
+		contentPanel.add(lblpDate);
 		
 		tfpDate = new JTextField();
 		tfpDate.setColumns(10);
-		tfpDate.setBounds(272, 169, 188, 25);
+		tfpDate.setBounds(256, 135, 158, 21);
 		contentPanel.add(tfpDate);
-		
-		JLabel lblPrice = new JLabel("\uAC00\uACA9 :");
-		lblPrice.setBounds(203, 216, 57, 15);
-		contentPanel.add(lblPrice);
 		
 		tfPrice = new JTextField();
 		tfPrice.setHorizontalAlignment(SwingConstants.RIGHT);
 		tfPrice.setColumns(10);
-		tfPrice.setBounds(272, 213, 105, 25);
+		tfPrice.setBounds(256, 179, 104, 21);
 		contentPanel.add(tfPrice);
 		
-		JLabel lblContents = new JLabel("\uCC45\uC18C\uAC1C");
-		lblContents.setBounds(24, 263, 57, 15);
+		JLabel lblPrice = new JLabel("가격:");
+		lblPrice.setBounds(191, 182, 57, 15);
+		contentPanel.add(lblPrice);
+		
+		JLabel lblContents = new JLabel("책 소개:");
+		lblContents.setBounds(12, 220, 57, 15);
 		contentPanel.add(lblContents);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setBounds(24, 536, 543, -247);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(12, 234, 560, 302);
 		contentPanel.add(scrollPane);
 		
-		tfContent = new JTextArea();
-		tfContent.setBounds(24, 288, 543, 261);
-		contentPanel.add(tfContent);
+		taContents = new JTextArea();
+		taContents.setLineWrap(true);
+		scrollPane.setViewportView(taContents);
 		
-		JButton btnInsert = new JButton("\uB4F1\uB85D");
-		btnInsert.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				InsertBook();
-			}
-
-			
-			private void InsertBook() {
-				// TODO Auto-generated method stub
-			
-				//DB 연동 sql 붙이기
-				try {
-					Class.forName("com.mysql.cj.jdbc.Driver");
-					Connection con = 
-							DriverManager.getConnection(
-									"jdbc:mysql://localhost:3306/sqlDB",
-									"root",
-									"12345");
-					Statement stmt = con.createStatement();			
-				
-					String sql = "insert into bookTBL values('";
-					sql = sql + tfISBN.getText()  + "','" + tfTitle.getText()  + "','";
-					sql = sql + tfAuthor.getText()  + "','" + tfPublisher.getText()  + "','";
-					sql = sql + tfpDate.getText()  + "','" + picURL + "',";
-					sql = sql + tfPrice.getText() + ",'"  + tfContent.getText().replaceAll("'", "&apos;") + "')";
-					System.out.println(sql);
-					if(stmt.executeUpdate(sql) > 0)
-						JOptionPane.showMessageDialog(null, "도서 등록 완료");
-					else
-						JOptionPane.showMessageDialog(null, "도서 등록 오류");
-													
-				} catch (ClassNotFoundException | SQLException e1) {
-					e1.printStackTrace();
-				}
-				
-				
-				
-				
-				
-				
-			}
-			
-			
-		});
-		btnInsert.setBounds(246, 567, 97, 25);
-		contentPanel.add(btnInsert);
-		
-		JButton btnCalendar = new JButton("날짜선택");
+		JButton btnCalendar = new JButton("날짜 선택");
 		btnCalendar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				WinCalendar winCalendar = new WinCalendar();
@@ -199,22 +198,73 @@ public class WinBookInsert extends JDialog {
 				tfpDate.setText(winCalendar.getDate());
 			}
 		});
-		btnCalendar.setBounds(470, 168, 97, 25);
+		btnCalendar.setBounds(421, 134, 97, 23);
 		contentPanel.add(btnCalendar);
 		
-		JLabel lblISBN = new JLabel("ISBN :");
-		lblISBN.setBounds(393, 216, 57, 15);
-		contentPanel.add(lblISBN);
+		JButton btnInsert = new JButton("등록");
+		btnInsert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				InsertBook();
+			}
+		});
+		btnInsert.setBounds(243, 546, 97, 23);
+		contentPanel.add(btnInsert);
+		
+		JLabel lblIsbn = new JLabel("ISBN:");
+		lblIsbn.setBounds(403, 182, 57, 15);
+		contentPanel.add(lblIsbn);
 		
 		tfISBN = new JTextField();
 		tfISBN.setHorizontalAlignment(SwingConstants.RIGHT);
 		tfISBN.setColumns(10);
-		tfISBN.setBounds(462, 213, 105, 25);
+		tfISBN.setBounds(468, 179, 104, 21);
 		contentPanel.add(tfISBN);
 		
-		JCheckBox ckOnce = new JCheckBox("\uD55C\uAD8C \uC785\uB825");
-		ckOnce.setSelected(true);
-		ckOnce.setBounds(452, 124, 115, 23);
+		ckOnce = new JCheckBox("한번 입력");
+		ckOnce.setBounds(440, 90, 115, 23);
 		contentPanel.add(ckOnce);
+	}
+
+	protected void InsertBook() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = 
+					DriverManager.getConnection(
+							"jdbc:mysql://localhost:3306/sqlDB",
+							"root",
+							"12345");
+			Statement stmt = con.createStatement();			
+			String sql = "insert into booktbl values('";
+			sql = sql + tfISBN.getText() + "','" + tfTitle.getText() + "','";
+			sql = sql + tfAuthor.getText() + "','" + tfPublisher.getText() + "','";
+			sql = sql + tfpDate.getText() + "','" + picURL + "',";
+			sql = sql + tfPrice.getText() + ",'" + taContents.getText().replaceAll("'", "&apos;") +"')" ; 
+			System.out.println(sql);
+			if(stmt.executeUpdate(sql) > 0) {
+				JOptionPane.showMessageDialog(null, "도서 등록 완료!!");
+				if(ckOnce.isSelected())
+					setVisible(false);
+				else
+					clearAll();
+			}
+			else
+				JOptionPane.showMessageDialog(null, "도서 등록 오류!!");
+			
+		} catch (ClassNotFoundException | SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+	}
+	private void clearAll() {
+		tfTitle.setText("");
+		tfAuthor.setText("");
+		tfPublisher.setText("");
+		tfpDate.setText("");
+		tfPrice.setText("");
+		tfISBN.setText("");
+		taContents.setText("");
+		picURL = "";
+		lblPic.setText(picURL);
+		tfTitle.requestFocus();
 	}
 }
---
+++ src/WinPublisher.java
@@ -0,0 +1,32 @@
+import java.awt.EventQueue;
+import java.util.Vector;
+
+import javax.swing.JDialog;
+import javax.swing.JList;
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class WinPublisher extends JDialog {
+	private String strPublisher;
+	public WinPublisher(Vector v) {
+		setTitle("출판사 선택");
+		setBounds(100, 100, 220, 262);
+		
+		JList lstPublisher = new JList();
+		lstPublisher.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				strPublisher = lstPublisher.getSelectedValue().toString();
+				setVisible(false);
+			}
+		});
+		getContentPane().add(lstPublisher, BorderLayout.CENTER);
+			
+		lstPublisher.setListData(v);
+	}
+	public String getPublisher() {
+		return strPublisher;
+	}
+
+}
